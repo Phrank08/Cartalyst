3a7011b70bc90e318f07c22fcf5418c5bf871fb4
Phrank08 <frankokey469@gmail.com>
2025-02-15 21:37:46 -0800

    Remove default test and add money tests

---
+++ Cartalyst main/tests-jasmine/moneyTest.js
@@ -0,0 +1,15 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+describe("Test suite: Format Currency", () =>{
+  it("convert cents to dollars", () =>{
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+
+  it("works with zero", ()=> {
+    expect(formatCurrency(0)).toEqual('0.00')
+  });
+
+  it("rounds up price to the nearest cents", ()=> {
+    expect(formatCurrency(2000.5)).toEqual('20.01');
+  })
+}) 
