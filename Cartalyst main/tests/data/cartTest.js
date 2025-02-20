00bcdb8ff8e6375de8eceaf3d31de561b5a31f19
Phrank08 <frankokey469@gmail.com>
2025-02-20 05:45:13 -0800

    Moved the setup code inside the beforeEach hook to share the code betwen the two tests.

--- Cartalyst main/tests/data/cartTest.js
+++ Cartalyst main/tests/data/cartTest.js
@@ -1,9 +1,12 @@
 import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
 
 describe("Test suite: addToCart", () => {
-  it("adds an existing product to the cart", ()=> {
+  beforeEach(() => {
     spyOn(localStorage, 'setItem');
+  })
 
+
+  it("adds an existing product to the cart", ()=> {
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -26,9 +29,6 @@ describe("Test suite: addToCart", () => {
   });
 
   it("adds a new product to the cart", ()=> {
-    spyOn(localStorage, 'setItem')
-
-
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([]);
     })
