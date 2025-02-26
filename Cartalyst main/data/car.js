b006746842aab99eeb55cbb48a19e948a3d42726
Phrank08 <frankokey469@gmail.com>
2025-02-26 00:35:55 -0800

    Added a method displayInfo() that console.log it's brand and model

--- Cartalyst main/data/car.js
+++ Cartalyst main/data/car.js
@@ -6,11 +6,15 @@ class Car {
     this.brand = brand;
     this.model = model; 
   }
+
+  displayInfo() {
+    console.log(`${this.brand} : ${this.model}`)
+  }
 }
 
 const car1 = new Car('Toyota', 'Corolla');
 const car2 = new Car('Tesla', 'Model 3');
 
 
-console.log(car1)
-console.log(car2);
+car1.displayInfo()
+car2.displayInfo();
