bfdd2eb6d2e76f406be36e8fb563b33ba6196d5c
Phrank08 <frankokey469@gmail.com>
2025-02-26 02:56:26 -0800

    Added a isTrunkOpen property, which tracks if the car's trunk is open.

--- Cartalyst main/data/car.js
+++ Cartalyst main/data/car.js
@@ -2,6 +2,7 @@ class Car {
   brand;
   model;
   speed = 0;
+  isTrunckOpen = false;
 
   constructor(brand, model) {
     this.brand = brand;
@@ -9,23 +10,37 @@ class Car {
   }
 
   displayInfo() {
-    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`)
+    const trunkStatus = this.isTrunckOpen ? 'opened' : 'closed'
+    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h, Trunk : ${trunkStatus}`)
   }
 
   go() {
-    this.speed += 5;
-    this.speed = Math.max(0, Math.min(this.speed, 200));
+    if(!this.isTrunckOpen && this.speed < 200) {
+      this.speed += 5;
+    }
   }
 
   brake() {
    this.speed = Math.max(0, this.speed - 5);
+  }
+
+  openTrunk() {
+    if(this.speed === 0) {
+      this.isTrunckOpen = true
+    }
+  }
+
+  closedTrunk() {
+    this.isTrunckOpen = false;
+
   }
 }
 
 const car1 = new Car('Toyota', 'Corolla');
 const car2 = new Car('Tesla', 'Model 3');
 
-car1.go();
-car2.brake();
-car1.displayInfo()
-car2.displayInfo();
+car1.go()
+car1.openTrunk();
+car1.displayInfo();
+
+//car2.displayInfo();
