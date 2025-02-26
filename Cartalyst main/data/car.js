d69d924703e8fed0ff602bc06addb955be196f4d
Phrank08 <frankokey469@gmail.com>
2025-02-26 01:39:35 -0800

    Added a speed property which should start from 0. And also be limited between 0 and 200.

--- Cartalyst main/data/car.js
+++ Cartalyst main/data/car.js
@@ -1,6 +1,7 @@
 class Car {
   brand;
   model;
+  speed = 0;
 
   constructor(brand, model) {
     this.brand = brand;
@@ -8,13 +9,23 @@ class Car {
   }
 
   displayInfo() {
-    console.log(`${this.brand} : ${this.model}`)
+    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`)
+  }
+
+  go() {
+    this.speed += 5;
+    this.speed = Math.max(0, Math.min(this.speed, 200));
+  }
+
+  brake() {
+   this.speed = Math.max(0, this.speed - 5);
   }
 }
 
 const car1 = new Car('Toyota', 'Corolla');
 const car2 = new Car('Tesla', 'Model 3');
 
-
+car1.go();
+car2.brake();
 car1.displayInfo()
 car2.displayInfo();
