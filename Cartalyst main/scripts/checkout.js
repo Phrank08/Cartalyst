2118fbbfa6de80a12d0d7433100bef881c4958a5
Phrank08 <frankokey469@gmail.com>
2025-03-03 04:10:37 -0800

    Convert from promises to async await

--- Cartalyst main/scripts/checkout.js
+++ Cartalyst main/scripts/checkout.js
@@ -6,6 +6,25 @@ import { loadCart } from "../data/cart.js";
 //import '../data/car.js';
 // import '../data/backend-practice.js';
 
+async function loadPage() {
+
+  await loadProductsFetch();
+
+  const value = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve('value1');
+    });
+  });
+
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+
+}
+
+loadPage()
+
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
@@ -20,6 +39,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 })
+  */
 
 /*
 
