3bf5b2f0510d8adb50017080bbb760d3051eeb86
Phrank08 <frankokey469@gmail.com>
2025-03-05 19:35:24 -0800

    Practiced adding XMLHttpRequest to fetch greeting from backend

--- Cartalyst main/scripts/checkout.js
+++ Cartalyst main/scripts/checkout.js
@@ -84,3 +84,11 @@ loadProducts(() => {
 })
 
 */
+
+const xhr = new XMLHttpRequest;
+
+xhr.addEventListener('load', () => {
+  console.log(xhr.response);
+})
+xhr.open('GET', 'https://supersimplebackend.dev/greeting');
+xhr.send();
